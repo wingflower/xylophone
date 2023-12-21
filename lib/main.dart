23ff6938e59991de5aae9332eb4e17771cd53963
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-21 17:15:42 +0900

    23_12_21 모든 건반 생성

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,40 @@ class Xylophone extends StatelessWidget {
         title: const Text('실로폰'),
       ),
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          gunban('도', Colors.red),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: gunban('도', Colors.red),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0),
+            child: gunban('레', Colors.orange),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0),
+            child: gunban('미', Colors.yellow),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: gunban('파', Colors.green),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 48.0),
+            child: gunban('솔', Colors.cyan),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 56.0),
+            child: gunban('라', Colors.blue),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 64.0),
+            child: gunban('시', Colors.purple),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 72.0),
+            child: gunban('도', Colors.orangeAccent),
+          ),
         ],
       ),
     );
@@ -44,6 +76,12 @@ class Xylophone extends StatelessWidget {
       width: 50,
       height: double.infinity,
       color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
